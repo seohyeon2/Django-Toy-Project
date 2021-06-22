1dc3fb6470d1f8f07f68f8882c11dcdeaf393a0a
박서현 <psh@bagseohyeon-ui-MacBook-Pro.local>
2021-06-22 18:39:15 +0900

    complete

---
+++ costaurant_complete/foods/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Menu',
+            fields=[
+                ('id', models.AutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=50)),
+                ('description', models.CharField(max_length=100)),
+                ('price', models.IntegerField()),
+                ('img_path', models.CharField(max_length=255)),
+            ],
+        ),
+    ]
