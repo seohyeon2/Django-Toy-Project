5dbb3e676b21a3173be0a3721270ebd2676ea94a
박서현 <psh@bagseohyeon-ui-MacBook-Pro.local>
2021-06-28 15:48:53 +0900

    datetime import

--- costaurant/menus/views.py
+++ costaurant/menus/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from datetime import datetime
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'menus/index.html')
+    today = datetime.now().date()
+    context = {'today': today}
+    return render(request, 'menus/index.html', context)
